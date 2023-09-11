e0f729d6cc9b2d80a95540ad385763b67c310130
kaique-oliveira <kaiqueoliveiraantonio@gmail.com>
2023-09-11 20:19:14 -0300

    Criado a função de pesquisar por nome

--- eventos/app/Http/Controllers/EventoController.php
+++ eventos/app/Http/Controllers/EventoController.php
@@ -8,9 +8,17 @@ use App\Models\Eventos;
 class EventoController extends Controller
 {
     public function index(){
-        $eventos = Eventos::all();
+        $search = request('search');
 
-        return View('evento.index', ['eventos' => $eventos]);
+        if($search){
+            $eventos = Eventos::where([
+                ['nome', 'like', '%'.$search.'%']
+            ])->get();
+        }else{
+            $eventos = Eventos::all();
+        }
+
+        return View('evento.index', ['eventos' => $eventos, 'search' => $search]);
     }
 
     public function detalhes($id){
