68e61d39a392e235b10ee69df9687e87ee9d29cd
kaique-oliveira <kaiqueoliveiraantonio@gmail.com>
2023-09-11 20:21:03 -0300

    Instalado o pacote de autenticação de login

--- eventos/routes/web.php
+++ eventos/routes/web.php
@@ -6,3 +6,12 @@ use App\Http\Controllers\EventoController;
 Route::get('/', [EventoController::class, 'index']);
 Route::post('/', [EventoController::class, 'store']);
 Route::get('/detalhes/{id}', [EventoController::class, 'detalhes']);
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
