b60fa3f21f749d781f971383d58350a4cbdf9d90
kaique-oliveira <kaiqueoliveiraantonio@gmail.com>
2023-09-10 22:49:47 -0300

    Criado a model e metodo index

--- eventos/app/Http/Controllers/EventoController.php
+++ eventos/app/Http/Controllers/EventoController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Eventos;
 
 class EventoController extends Controller
 {
-    //
+    public function index(){
+        $eventos = Eventos::all();
+
+        return View('evento.index', ['eventos' => $eventos]);
+    }
 }
