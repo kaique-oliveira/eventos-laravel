e909bf46aff632f6cae30f27d7e6bdafeec0c922
kaique-oliveira <kaiqueoliveiraantonio@gmail.com>
2023-09-11 20:35:46 -0300

    Criado link de acesso

--- eventos/resources/views/layouts/main.blade.php
+++ eventos/resources/views/layouts/main.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,16 +13,20 @@
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
 
     <link rel="stylesheet" href="/css/global.css">
 </head>
+
 <body>
     <header>
         <div class="container-logo">
             <a href="/"><ion-icon name="today-sharp"></ion-icon></a>
-        </div>      
+        </div>
 
         <nav class="navbar navbar-expand-sm navbar-dark" style="background-color: #2e2b41;">
             <div class="collapse navbar-collapse" id="navbarNav">
@@ -29,16 +34,33 @@
                     <li class="nav-item active">
                         <a class="nav-link" href="/">Eventos</a>
                     </li>
+                    @auth
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Entrar</a>
+                        <a class="nav-link" href="/dashboard">Meus eventos</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/perfil">Perfil</a>
+                      <form action="/logout" method="POST">
+                        @csrf
+                        <a href="/logout"
+                            class="nav-link"
+                            onclick="event.preventDefault();
+                            this.closest('form').submit();">Sair</a>
+                      </form>
                     </li>
+                    @endauth
+                    @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="/login">Entrar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/register">Cadastrar-se</a>
+                    </li>
+                    @endguest
                 </ul>
             </div>
         </nav>
     </header>
     @yield('content')
 </body>
-</html>     
+
+</html>
