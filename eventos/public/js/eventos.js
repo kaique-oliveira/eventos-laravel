822c3627a7de75208b41256a7ffaa784550551b6
kaique-oliveira <kaiqueoliveiraantonio@gmail.com>
2023-09-12 22:06:50 -0300

    Criado interação

--- eventos/public/js/eventos.js
+++ eventos/public/js/eventos.js
@@ -3,11 +3,47 @@
 const _btn_criar_evento = document.querySelector("#btn-criar");
 const _btn_fechar_modal = document.querySelector("#btn-fechar");
 const _modal_criar_evento = document.querySelector("#modal-criar-evento");
+const _link_criar_evento = document.querySelector("#link-criar");
+const _modal_editar_evento = document.querySelector("#btn-fechar-editar");
 
-_btn_criar_evento.addEventListener("click", () => {
-    _modal_criar_evento.className = "modal-wrapper-show";
-});
 
-_btn_fechar_modal.addEventListener("click", () => {
-    _modal_criar_evento.className = "modal-wrapper-hide";
-});
+if(_btn_criar_evento){
+    _btn_criar_evento.addEventListener("click", () => {
+        _modal_criar_evento.className = "modal-wrapper-show";
+    });
+}
+
+
+if(_btn_fechar_modal){
+    _btn_fechar_modal.addEventListener("click", () => {
+        _modal_criar_evento.className = "modal-wrapper-hide";
+    });
+}
+
+
+if( _link_criar_evento){
+    _link_criar_evento.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        localStorage.setItem("isCriar", true);
+        window.location.href = "/";
+    });
+}
+
+function abrirModal(){
+    const isCriar = localStorage.getItem("isCriar");
+
+    if(isCriar && isCriar == 'true'){
+        _modal_criar_evento.className = "modal-wrapper-show";
+        localStorage.setItem("isCriar", false);
+    }
+}
+
+abrirModal();
+
+
+if(_modal_editar_evento){
+    _modal_editar_evento.addEventListener('click', () => {
+        window.location.href = "/dashboard";
+    })
+}
